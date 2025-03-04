03cb5d5cfe404cde74e68a3a63f3eaf9c19b1b2e
1VishalSD <ilearnvk@gmail.com>
2025-03-04 09:27:51 +0530

    initalize the file for building school management system serverless

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-import adminRouter from "./routes/admin.route";
-
-const app = new Hono();
-app.route("/admin", adminRouter);
-
-// Export Hono app for Cloudflare Workers
-export default app;
--- backend/src/routes/admin.route.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-import { addStudent } from "../controllers/admin.controllers";
-
-const adminRouter = new Hono();
-
-adminRouter.post("/add_student", addStudent);
-
-export default adminRouter;
